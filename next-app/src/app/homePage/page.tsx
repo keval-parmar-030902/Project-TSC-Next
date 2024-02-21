90afec2d0f021bc88e1400b5960eed0874161a2c
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-02-21 16:15:10 +0530

    create route logic

---
+++ next-app/src/app/homePage/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Home = (props: Props) => {
+    return (
+        <div>
+            <h1>This is home page
+            </h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ next-app/src/app/shopPage/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Shop = (props: Props) => {
+    return (
+        <div>
+            <h1>This is Shop page
+                
+            </h1>
+        </div>
+    )
+}
+
+export default Shop
