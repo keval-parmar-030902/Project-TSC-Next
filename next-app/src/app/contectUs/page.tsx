8b27570be88d55824b992650759f7c738ea60649
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-02-26 12:28:43 +0530

    div set

--- next-app/src/app/contectUs/page.tsx
+++ next-app/src/app/contectUs/page.tsx
@@ -1,48 +1,42 @@
 import React from "react";
-import Style from './page.module.css'
+import Style from "./page.module.css";
 import Form from "@/components/Form";
 import Buddy from "@/components/Buddy";
 
-interface Props { }
-import Banner from "@/components/Banner"
+interface Props {}
+import Banner from "@/components/Banner";
 import SidePanel from "@/components/SidePanel";
-interface Props { }
-import Footer from "@/components/footer"
+interface Props {}
+import Footer from "@/components/footer";
 import HamburgerMenu from "@/common/hamburger";
 
-interface Props { }
+interface Props {}
 
 const Contectus = (props: Props) => {
   return (
     <>
-
-      <div className={Style.main}>
-        <HamburgerMenu />
-        <div className={Style.toggle}>
-          <div className={Style.main}>
-            <div className={Style.side}>
-              <SidePanel />
-            </div>
-            <div>
-              <Banner />
-              <div className={Style.contactus}>
-
-
-                <div className={Style.heading}>
-                  <h1>Personal Information</h1>
-                </div>
-                <div className={Style.formsection}>
-                  <Form />
-                  <Buddy />
-                </div>
+      <HamburgerMenu />
+      <div className={Style.toggle}>
+        <div className={Style.main}>
+          <div className={Style.side}>
+            <SidePanel />
+          </div>
+          <div>
+            <Banner />
+            <div className={Style.contactus}>
+              <div className={Style.heading}>
+                <h1>Personal Information</h1>
               </div>
-              <Footer />
+              <div className={Style.formsection}>
+                <Form />
+                <Buddy />
+              </div>
             </div>
+            <Footer />
           </div>
         </div>
       </div>
     </>
-
   );
 };
 
