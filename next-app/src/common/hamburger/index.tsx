a2b22208249c47610cc0ffefccbfa8d6cf31ce0b
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-02-26 12:26:47 +0530

    hamburger icon set

--- next-app/src/common/hamburger/index.tsx
+++ next-app/src/common/hamburger/index.tsx
@@ -1,27 +1,39 @@
 "use client";
-import React, { useState } from 'react';
-import styles from './hamburger.module.css';
-import SidePanel from '@/components/SidePanel';
+import React, { useState } from "react";
+import styles from "./hamburger.module.css";
+import SidePanel from "@/components/SidePanel";
+import close from '@/public/close.svg'
+import Image from 'next/image'
+import { FaBars } from "react-icons/fa6";
 
 const HamburgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className={styles.hamburgermenu}>
-            <div className={isOpen ? styles.menuopen : styles.hamburgericon} onClick={toggleMenu}>
+  return (
+    <div className={styles.hamburgermenu}>
+      <div
+        className={isOpen ? styles.menuopen : styles.hamburgericon}
+        onClick={toggleMenu}
+      >
+        {/* <div className={styles.line}></div>
                 <div className={styles.line}></div>
-                <div className={styles.line}></div>
-                <div className={styles.line}></div>
-            </div>
-            <div className={isOpen ? styles.menuopen : styles.menu}>
-                <SidePanel />
-            </div>
-        </div >
-    );
+                <div className={styles.line}></div> */}
+        {isOpen === true ? (
+          
+          <Image src={close} alt={"logo"}></Image>
+        ) : (
+            <FaBars/>
+        )}
+      </div>
+      <div className={isOpen ? styles.menuopen : styles.menu}>
+        <SidePanel />
+      </div>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
